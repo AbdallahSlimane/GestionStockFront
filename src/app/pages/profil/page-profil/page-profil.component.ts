9f4eb797ca3a30e84940c47186006b96a8e8e804
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2022-04-30 22:49:19 +0100

    developpement de interface changer mot de passe

--- src/app/pages/profil/page-profil/page-profil.component.ts
+++ src/app/pages/profil/page-profil/page-profil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-page-profil',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PageProfilComponent implements OnInit {
 
-  constructor() { }
+  constructor( private router:Router) { }
 
   ngOnInit(): void {
+  }
+
+  changermotdepasse(): void{
+    this.router.navigate(["changermotdepasse"]);
   }
 
 }
