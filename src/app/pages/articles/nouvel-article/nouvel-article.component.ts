644267560cce8ca73c54e0ba5944827ed5369c6f
Ahmed Tiba <ahmed.tiba.1993@gmail.com>
2022-05-26 12:06:35 +0100

    Création d'un nouvel article

--- src/app/pages/articles/nouvel-article/nouvel-article.component.ts
+++ src/app/pages/articles/nouvel-article/nouvel-article.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ArticleService } from 'src/app/services/article/article.service';
+import { CategoryService } from 'src/app/services/category/category.service';
+import { ArticleDto, CategoryDto } from 'src/gs-api/src/models';
 
 @Component({
   selector: 'app-nouvel-article',
@@ -8,12 +11,38 @@ import { Router } from '@angular/router';
 })
 export class NouvelArticleComponent implements OnInit {
 
-  constructor(private router:Router) { }
+  articleDto : ArticleDto = {};
+  categorieDto : CategoryDto = {};
+  listCategorie : Array<CategoryDto> = [];
+  errorMsg : Array<string> = [];
+
+  constructor(
+    private router:Router,
+    private activatedRoute : ActivatedRoute,
+    private articlesService : ArticleService,
+    private categoryService : CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.categoryService.findAll()
+    .subscribe(categories =>{
+      this.listCategorie = categories;
+    });
   }
 
   cancel():void{
     this.router.navigate(["articles"]);
+  }
+
+  enregistrerArticle() : void{
+    if(this.categorieDto.id){
+      this.articleDto.category = this.categorieDto;
+    }
+    this.articlesService.enregistrerArticle(this.articleDto)
+    .subscribe( art => {
+      this.router.navigate(['articles']);
+    },error =>{
+      this.errorMsg = error.error.errors;
+    })
   }
 }
---
+++ src/app/services/article/article.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { ArticleDto } from 'src/gs-api/src/models';
+import { ArticleControllerService } from 'src/gs-api/src/services';
+import { UserService } from '../user/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ArticleService {
+
+  constructor(
+    private userService : UserService,
+    private articleService : ArticleControllerService
+  ) { }
+
+  enregistrerArticle(articleDto : ArticleDto) : Observable<ArticleDto>{
+    articleDto.idEntreprise = this.userService.getConnectedUser().entreprise?.id;
+    return this.articleService.saveUsingPOST(articleDto);
+  }
+
+  findAllArticle() : Observable<ArticleDto[]>{
+    return this.articleService.findAllUsingGET();
+  }
+
+  findArticleById(idArticle? : number) : Observable<ArticleDto>{
+    if(idArticle){
+      return this.articleService.findByIdUsingGET(idArticle);
+    }
+
+    return of();
+  }
+}
